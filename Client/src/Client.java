52c0f1e14d8fe4a9167521025e7e8d742272cd07
kweera2 <kweera2@uic.edu>
2019-04-22 17:48:52 -0500

    eugenio's version

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,70 +1,48 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.util.Scanner;
+import java.io.Serializable;
+import java.util.function.Consumer;
 
-public class Client extends Thread {
-    String ipAddress;
-    int port;
+public class Client extends ClientConnection {
 
-    //Scanner scn = new Scanner(System.in );
-    InetAddress ip;
-    Socket S;
-    DataInputStream dis;
-    DataOutputStream dos;
+	private String ip;
+	private int port;
+	private String name;
+	private String hand;
+	
+	public Client(String ip, int port, Consumer<Serializable> callback, String name) {
+		super(callback);
+		this.ip = ip;
+		this.port = port;
+		this.name = name;
+	}
 
-    String txData = "";//output data
-    String rxData = "";//input data
 
-    //read string
-    public String readData(){
-	String temp = this.rxData;
-	this.rxData = "";
-        return temp;
-    }
+	@Override
+	protected boolean isServer() {
+		return false;
+	}
 
-    //send string
-    public void sendData(String data){
-        this.txData = data;
-    }
+	@Override
+	protected String getIP() {
+		// TODO Auto-generated method stub
+		return this.ip;
+	}
 
-    //inin port
-    //FIXME implement boolean that return true if connection is successful
-    public void init(String ipAddress,int port){
-        this.ipAddress = ipAddress;
-        this.port = port;
-    }
+	@Override
+	protected int getPort() {
+		// TODO Auto-generated method stub
+		return this.port;
+	}
 
-    //Thread for client
-    public void run(){
-        try{
-            ip = InetAddress.getByName(this.ipAddress);
-            S = new Socket(ip, port);
-            dis = new DataInputStream(S.getInputStream());
-            dos = new DataOutputStream(S.getOutputStream());
+	@Override
+	protected String getName() {
 
-            while(true){
-                if(!txData.isEmpty() ){
-                    dos.writeUTF(txData);
-                    txData = "";
-                }
-                if(dis.available()>0){
-                    rxData = dis.readUTF();
-                    //System.out.println("client: "+rxData);
-                }
-                //if "Exit" is string, close connection
-                if(rxData.equals("Exit")){
-                    S.close();
-                    break;
-                }
-            }
-            //scn.close();
-            dis.close();
-            dos.close();
-        }
-        catch(Exception E){
-            E.printStackTrace();
-        }
-    }
+		return this.name;
+	}
+
+	@Override
+	protected String getHand() {
+
+		return this.hand;
+	}
+
 }
